98706a3c923e60f31a913e8a7e6872a7a92c5c5f
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2021-10-13 23:10:35 +0100

    Should be ipv6 now, trying to get channels working
    This bad boy should be able to allow ipv6 now, plus starting to add channels but it is very confusing.

--- serverScript.py
+++ serverScript.py
@@ -2,18 +2,25 @@
 import socket
 
 PORT = 6667
-HOST = '127.0.0.1'
+HOST = '::1'
+numberOfClients = 3 #numbers of clients allowed to connect
 
 #Creates a socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
-#Binds the created socket to the port
+
+
 server_adderess = (HOST, PORT)
 print('Starting up on {} port {}'.format(*server_adderess))
+
+#Allows to reuse the socket. Big POG.
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+#Binds the created socket to the port
 sock.bind(server_adderess)
 
 #Allows for 2 clients
-sock.listen(2)
+sock.listen(numberOfClients)
 
 
 #Will wait for someone/thing to connect
@@ -26,14 +33,11 @@ while True:
 		
 		#Gets the data and prints it.
 		while True:
-			data = connection.recv(16)
-			print('recived {!r}'.format(data))
-			if data:
-				print('Sending data back to client')
-				connection.sendall(data)
-			else:
-				print('No data from', client_address)
+			data = connection.recv(1024)
+			if not data:
 				break
+			connection.sendall(data)
+			
 				
 	#Closes the connection
 	finally:
@@ -41,4 +45,60 @@ while True:
 		connection.close()
 
 
-			
+
+
+###THIS IS ME TRYING TO FIGURE OUT HOW CHANNELS AND WHAT NOT WORKS...###
+
+#Class to hold all of the channel details
+class Channel:
+	def __init__(self, name, topic='No Topic'):
+		self.name = name
+		self.topic = topic
+		self.clients = Set["Clients"] = set()
+
+class Client:
+	def __init__(self, request, client_adress, server):
+		self.user = None
+		self.host = client_address
+		self.nick
+		self.send_queue = []
+		self.channels = {}
+		
+		
+	def joinChannel():
+		channel = self.server.Channels.setdefault(r_channel_name, channel(r_channel_name)
+		channel.clients.add(self)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
