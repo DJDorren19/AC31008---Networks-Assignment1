b4baf1feaf218b61c637dde680ce394b78f9e835
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2021-10-14 19:28:41 +0100

    ServerScript
    You can create a channel now, but only through code at the moment. It is a bit convoluted but should at least help with some things

--- serverScript.py
+++ serverScript.py
@@ -2,19 +2,55 @@
 import socket
 
 PORT = 6667
-HOST = '127.0.0.1'
+HOST = '::1'
+numberOfClients = 3 #numbers of clients allowed to connect
 
 #Creates a socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
-#Binds the created socket to the port
 server_adderess = (HOST, PORT)
-print('Starting up on {} port {}'.format(*server_adderess))
+print('Starting up on', HOST , 'PORT', PORT)
+
+#Allows to reuse the socket. Big POG and binds scoket to port.
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(server_adderess)
 
-#Allows for 2 clients
-sock.listen(2)
+#Allows for an x number of clients
+sock.listen(numberOfClients)
 
+
+#-----------------------------------------------------------------------------------------
+
+#Class to hold all of the channel details
+class Channel:
+	
+	#Sets up the object
+	def __init__(self, name, topic):
+		self.name = name
+		self.clientList = [] #List to hold clients 
+		self.topic = topic
+		
+
+#-----------------------------------------------------------------------------------------
+
+#Class to hold all of the client details
+class Clients:
+	
+	#Sets up the object
+	def __init__(self, username, host):
+		self.username = username
+		self.host = host
+		
+#-----------------------------------------------------------------------------------------
+
+channelList = [] #Stores all the channels
+
+#Creates a new channel and then stores it in a list
+def createChannel(name, topic):
+	newChannel = Channel(name, topic)
+	channelList.append(newChannel)
+
+#-----------------------------------------------------------------------------------------
 
 #Will wait for someone/thing to connect
 while True:
@@ -24,21 +60,74 @@ while True:
 	try:
 		print('Connection from', client_address)
 		
+		
+		########################TEST CODE FOR CHANNEL########################
+		
+		newClient = Clients("Daniel", client_address)
+		
+		createChannel("#test", "Bababooey")
+		
+		channelList[0].clientList.append(newClient)
+		
+		print("Channel: ", channelList[0].name)
+		
+		for i in channelList[0].clientList:
+			print("User: ", i.username, "Host: ", i.host)
+		
+		######################################################################
+	
+		
+
 		#Gets the data and prints it.
 		while True:
-			data = connection.recv(16)
-			print('recived {!r}'.format(data))
-			if data:
-				print('Sending data back to client')
-				connection.sendall(data)
-			else:
-				print('No data from', client_address)
+			data = connection.recv(1024)
+			
+			print(str(data))
+			if not data:
 				break
-				
+			connection.sendall(data)
+					
 	#Closes the connection
 	finally:
 		print("Closing current connection")
 		connection.close()
 
+#-----------------------------------------------------------------------------------------
+
+
+
+
+      		
 
 			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
